f60d8b8e6cdbcacf682cfce148791c6667a907cd
CurtisVebraak <0896588@hr.nl>
2015-10-05 13:24:55 +0200

    correct

--- A4_1_Warmup/A4_1_Warmup/A4_1_Warmup.py
+++ A4_1_Warmup/A4_1_Warmup/A4_1_Warmup.py
@@ -1,7 +1,7 @@
 ﻿## warmup
 
 ## Fahrenheit > Celsius
-fahrenheit = int(raw_input('Enter Fahrenheit: \n'))
+fahrenheit = int(raw_input('(F>C) Enter Fahrenheit: \n'))
 
 celsius = (float(fahrenheit - 32) * 5/9)
 celsius = round(celsius, 2)
@@ -9,14 +9,14 @@ celsius = round(celsius, 2)
 print celsius, "celsius \n"
 
 ## Celsius > Kelvin
-celsius = int(raw_input('Enter celcius: \n'))
+celsius = int(raw_input('C>K) Enter celcius: \n'))
 kelvin = (float(celsius + 273.15))
 if kelvin < 0:
-    print "Kelvin: 0 is absolute zero"
+    print('Kelvin: 0 is absolute zero, your outcome was: ' + str(kelvin))
 else:
     print kelvin, "Kelvin \n"
 
 ## absolute value
-number = raw_input('Enter a number: ')
+number = raw_input('(ABS) Enter a number: ')
 number = float(number)
 print('The absolute value is ' + str(abs(number)))
