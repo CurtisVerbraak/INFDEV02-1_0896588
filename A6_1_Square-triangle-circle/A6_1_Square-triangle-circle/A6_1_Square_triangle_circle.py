9caaf6bec0b46eb19335c5e1a020c6fde2b0da94
CurtisVebraak <0896588@hr.nl>
2015-10-06 15:33:43 +0200

    test

--- A6_1_Square-triangle-circle/A6_1_Square-triangle-circle/A6_1_Square_triangle_circle.py
+++ A6_1_Square-triangle-circle/A6_1_Square-triangle-circle/A6_1_Square_triangle_circle.py
@@ -5,22 +5,35 @@ width = 5
 for i in range(0,5):
     print("*" * width)
 
-#hollow square
-print ("\n \n")
 
+##---------------------
+print ("\n \n")
+##---------------------
+
+#hollow square
 x = "*"
 t = " "
 
-for i in range(0,5):
-    if i == 0 or i == 4:
+for i in range(0,4):
+    if i == 0 or i == 3:
         var = x * 5
     else:
-        var = x + t * 4 + x
+        var = x + t * 3 + x
     for j in range(0,1):
         print(var)
 
 #triangle
 for i in range(0,5):
     var = x * i 
+    for j in range(0,1):
+        print(var)
+
+##--------------------- 
+print ("\n \n")
+##---------------------
+
+#piramide
+for i in range(0,5,-1):
+
     for j in range(0,1):
         print(var)
