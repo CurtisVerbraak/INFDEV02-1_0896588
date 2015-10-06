c4bbd943b090a8cccf46cd924cde5df0dd835ca7
curtisverbraak <0896588@hr.nl>
2015-10-06 23:48:58 +0200

    tr

--- A5_3_Cryptography/A5_3_Cryptography/A5_3_Cryptography.py
+++ A5_3_Cryptography/A5_3_Cryptography/A5_3_Cryptography.py
@@ -1,1 +1,9 @@
-## Cryptography
+﻿## Cryptography
+
+input = raw_input('Enter some text to encrypt: ')
+shift = raw_input('Enter a number: ')
+
+shift = int(shift)%26
+
+if len(input) > 0:
+    print shift
